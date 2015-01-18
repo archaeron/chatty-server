7f8e7a5921b91f4f27dfda85884f9e0c133d8763
archaeron <whatamaps@gmail.com>
2015-01-18 16:26:01 +0100

    nix possibly working

--- shell.nix
+++ shell.nix
@@ -1,14 +1,16 @@
 let pkgs = (import <nixpkgs> {});
   haskellPackages = pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
-  extension = self : super :
-  let callPackage = self.callPackage;
-  in {
+    extension = self : super :
+    let callPackage = self.callPackage;
+    in {
     # (1)
-    prettyCompact = callPackage ./nix/pretty-compact.nix {};
+      groundhog = callPackage ./nix/groundhog.nix {};
+      groundhogSqlite = callPackage ./nix/groundhog_sqlite.nix {};
+      groundhogTh = callPackage ./nix/groundhog_th.nix {};
       thisPackage = haskellPackages.callPackage (import ./default.nix) {};
-  };});
+    };
+  }
+);
 in pkgs.lib.overrideDerivation haskellPackages.thisPackage (old: {
-  buildInputs = old.buildInputs ++ [
-    haskellPackages.cabalInstall
-  # (2)
-  ];})
+  }
+)
